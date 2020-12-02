a06c5333ffe3a7693d1083477b7b099ad52cc63c
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 01:46:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,9 +19,9 @@ def sort_array_char_count(array)
   array.sort do |a, b|
     if a.length == b.length
       0
-    elsif a.length > b.length
-      -1
     elsif a.length < b.length
+      -1
+    elsif a.length > b.length
       1
     end
   end
